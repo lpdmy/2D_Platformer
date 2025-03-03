8b729e8484f266a658bd9080371d7cfbdd3bec8e
BPMinh6624 <minhntde180279@fpt.edu.vn>
2025-03-03 16:23:16 +0700

    Folder 11

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    public float waitToRespawn;
+
+    public int gemsCollected;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RespawnPlayer()
+    {
+        StartCoroutine(RespawnCo());
+    }
+
+    private IEnumerator RespawnCo()
+    {
+        PlayerController.instance.gameObject.SetActive(false);
+
+        AudioManager.instance.PlaySFX(8);
+
+        yield return new WaitForSeconds(waitToRespawn - (1f/UIController.instance.fadeSpeed));
+
+        UIController.instance.FadeToBlack();
+
+        yield return new WaitForSeconds((1f / UIController.instance.fadeSpeed) + 0.2f);
+
+        UIController.instance.FadeFromBlack();
+
+        PlayerController.instance.gameObject.SetActive(true);
+
+        PlayerController.instance.transform.position = CheckpointController.instance.spawnPoint;
+
+        PlayerHealthController.instance.currentHealth = PlayerHealthController.instance.maxHealth;
+        UIController.instance.UpdateHealthDisplay();
+
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu instance;
+
+    public string levelSelect, mainMenu;
+
+    public GameObject pauseScreen;
+
+    public bool isPaused;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseUnpause();
+        }
+    }
+
+    public void PauseUnpause()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            isPaused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(levelSelect);
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+        Time.timeScale = 1f;
+    }
+}
