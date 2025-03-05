368fe396397ff8c86fb145c5dba7a1c32536ca6f
lpdmy <144099333+lpdmy@users.noreply.github.com>
2025-03-06 01:30:44 +0700

    Moving around level select

---
+++ Assets/Scripts/LevelSelectPlayer.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class LevelSelectPlayer : MonoBehaviour
+{
+    public MapPoint currentPoint;
+
+    public float moveSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.transform.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currentPoint.transform.position) < .1f)
+        {
+
+        if (Input.GetAxisRaw("Horizontal") > .5f)
+        {
+            if (currentPoint.right != null)
+            {
+                SetNextPoint(currentPoint.right);
+            }
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < -.5f)
+        {
+            if (currentPoint.left != null)
+            {
+                SetNextPoint(currentPoint.left);
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") > .5f)
+        {
+            if (currentPoint.up != null)
+            {
+                SetNextPoint(currentPoint.up);
+            }
+        }
+
+        if (Input.GetAxisRaw("Vertical") < -.5f)
+        {
+            if (currentPoint.down != null)
+            {
+                SetNextPoint(currentPoint.down);
+            }
+        }
+        }
+    }
+
+    public void SetNextPoint(MapPoint nextPoint)
+    {
+        currentPoint = nextPoint;
+    }
+}
