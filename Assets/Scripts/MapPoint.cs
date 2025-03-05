fe9ad703b11fbb786d5bf743840b926d8216d18c
lpdmy <144099333+lpdmy@users.noreply.github.com>
2025-03-06 01:08:00 +0700

    Setting up map point

---
+++ Assets/Scripts/MapPoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    public MapPoint up, right, down, left;
+    public bool isLevel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
